47c93e140f0d793d5ff8f6992860c23e9df417c6
eloisezheng <zlyisme@gmail.com>
2023-06-18 13:34:52 -0700

    array questions all done

--- group_anagram.py
+++ group_anagram.py
@@ -61,4 +61,123 @@ class Solution:
         return res
 
 
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        cols = collections.defaultdict(set)
+        rows = collections.defaultdict(set)
+        # key of squares: pairs (r/3, c/3)
+        # defaultdict(set) creates a dictionary-like object where each key has default value of an empty set.
+        squares = collections.defaultdict(set)
+        
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if (board[r][c] in rows[r] or 
+                    board[r][c] in cols[c] or 
+                    board[r][c] in squares[(r//3, c//3)]):
+                    return False
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r//3, c//3)].add(board[r][c])
+        return True
 
+class Codec:
+    def encode(self, strs: List[str]) -> str:
+        """Encodes a list of strings to a single string.
+        """
+        res = ""
+        for word in strs:
+            res += str(len(word)) + "#" + word
+        print(res)
+        return res
+        
+
+    def decode(self, s: str) -> List[str]:
+        """Decodes a single string to a list of strings.
+        """
+        res, i = [], 0
+        while i < len(s):
+            j = i
+            while s[j] != "#":
+                j += 1
+            # use slicing to find out the length of word
+            # 从i到j-1， 不包括j
+            # j位置是#
+            length = int(s[i:j])
+            res.append(s[j+1: j+1 +length])
+            i = j + 1 + length
+        return res
+        
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        # iterate nums, check number -1不在numSet, Set length为0,再记录length最长的长度
+        numSet = set(nums)
+        longest = 0
+        for number in nums:
+            if number - 1 not in numSet:
+                length = 0
+                while number + length in numSet:
+                    length += 1
+                longest = max(longest, length)
+        return longest
+
+class RandomizedSet:
+
+    def __init__(self):
+        self.numMap = {} # used to map each element's index in numList 
+        self.numList = [] # store val
+
+    def insert(self, val: int) -> bool:
+        res = val not in self.numList
+        if res:
+            self.numMap[val] = len(self.numList)
+            self.numList.append(val)
+        return res
+        
+    # delete之后，list的最后一个copy到要delete的地方，然后pop list
+    def remove(self, val: int) -> bool:
+        res = val in self.numList
+        if res:
+            idx = self.numMap[val]
+            lastValue = self.numList[-1]
+            self.numList[idx] = self.numList[-1]
+            self.numList.pop()
+            # remap hashMap to match last element's index of list 
+            self.numMap[lastValue] = idx
+            del self.numMap[val]
+        return res
+  
+    def getRandom(self) -> int:
+        return random.choice(self.numList)
+    
+class Solution:
+    def minStartValue(self, nums: List[int]) -> int:
+        # iterate and add number in nums，找到最小值，然后return负值+1
+        total = 0
+        minVal = 0
+        for number in nums:
+            total += number
+            print(total)
+            minVal = min(minVal, total)
+        return (-minVal + 1)
+
+
+###### Two pointers
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        #The isalnum() method returns True if all the characters are alphanumeric, meaning alphabet letter (a-z) and numbers (0-9).
+        #use "while" for case： s[left] has 2个连续 Non-AlphNumeric strings but s[right] has 1.
+        left, right = 0, len(s)- 1
+        while left < right:
+            while left< right and not s[left].isalnum():
+                left += 1
+            while left< right and not s[right].isalnum():
+                right -= 1
+            print(s[left].lower(),s[right].lower())
+            if s[left].lower() != s[right].lower():
+                return False
+            left += 1
+            right -= 1
+        return True
+        
